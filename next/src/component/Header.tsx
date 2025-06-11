361119b2411e2f3921803995ec4c44979e40bda4
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 01:56:42 +0300

    feat: update Settings component to use dynamic navigation based on current path

--- next/src/component/Header.tsx
+++ next/src/component/Header.tsx
@@ -1,6 +1,8 @@
+"use client";
 import style from "@/style/Header.module.css";
 import { Settings as SettingsIcon } from "@deemlol/next-icons";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
     return (
@@ -13,8 +15,11 @@ const Header = () => {
 export default Header;
 
 const Settings = () => {
+    const pathName = usePathname();
+    const href = pathName.startsWith('/settings') ? '/' : '/settings';
+
     return (
-        <Link href={"/settings"} className={style.settings}>
+        <Link href={href} className={style.settings}>
             <SettingsIcon size={24} color="#FFFFFF" />
         </Link>
     )
