d5fc5add16783f112471e0d997c9b2ea2d07000c
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 01:49:07 +0300

    feat: implement Settings component in Header for navigation to settings page

--- next/src/component/Header.tsx
+++ next/src/component/Header.tsx
@@ -1,11 +1,21 @@
 import style from "@/style/Header.module.css";
+import { Settings as SettingsIcon } from "@deemlol/next-icons";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header className={style.header}>
-            HHcf
+            <Settings />
         </header>
     )
 };
 
 export default Header;
+
+const Settings = () => {
+    return (
+        <Link href={"/settings"} className={style.settings}>
+            <SettingsIcon size={24} color="#FFFFFF" />
+        </Link>
+    )
+}
