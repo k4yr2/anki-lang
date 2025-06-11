e3c1353f814c455304342f03e05dda73ad6ceec4
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 01:43:42 +0300

    feat: add Header component to RootLayout for improved navigation

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/style/global.css";
+import Header from "@/component/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
