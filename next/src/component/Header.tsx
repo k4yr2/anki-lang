46fd56d9874d21e4934ce930bb0c64582c9a49ea
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 01:43:39 +0300

    feat: add Header component with associated styles

---
+++ next/src/component/Header.tsx
@@ -0,0 +1,11 @@
+import style from "@/style/Header.module.css";
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            HHcf
+        </header>
+    )
+};
+
+export default Header;
