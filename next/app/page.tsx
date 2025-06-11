690c217325980d9b0df2f5d7e03e8101889abb6d
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 01:03:18 +0300

    feat: add settings page with navigation link to home

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       Home 314
+      <Link href="/settings">Settings</Link>
     </div>
   );
 }
---
+++ next/app/settings/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Link from "next/link";
+
+export default function SettingsPage() {
+  return (
+    <div>
+        Settings 696
+        <Link href="/">Home</Link>
+    </div>
+  );
+}
