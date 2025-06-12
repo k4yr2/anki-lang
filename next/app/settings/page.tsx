bb44e9f2b3354834c07dfbc6e34414dcb16779c8
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 21:24:48 +0300

    feat: replace username input with OpenAI Key input in SettingsPage

--- next/app/settings/page.tsx
+++ next/app/settings/page.tsx
@@ -1,16 +1,21 @@
 "use client";
-import { FormControl, FormLabel, Input } from "@mui/joy";
+import { FormControl, FormLabel, Grid, Input } from "@mui/joy";
+import { SxProps } from "@mui/joy/styles/types";
 import Link from "next/link";
 
 export default function SettingsPage() {
+  const labelSx : SxProps = { display: 'flex', justifyContent: 'right' };
+
   return (
     <div>
-        Settings 696
-        <Link href="/">Home</Link>
-        <FormControl>
-          <FormLabel>Username</FormLabel>
-          <Input />
-        </FormControl>
+        <Grid container rowSpacing={1} columnSpacing={2} alignItems="center">
+          <Grid xs={2} sx={labelSx}>
+            <FormLabel>OpenAI Key</FormLabel>
+          </Grid>
+          <Grid xs={10}>
+            <Input placeholder="Enter your API Key" />
+          </Grid>
+        </Grid>
     </div>
   );
 }
