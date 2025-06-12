13b176f616b7c2ec24e025b3479f6f8b15bbaf0a
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 04:58:49 +0300

    feat: add CssBaseline to Content component for improved styling and client-side rendering

--- next/src/component/Content.tsx
+++ next/src/component/Content.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState, useEffect } from "react";
 
 const Content = ({ children } : { children : React.ReactNode}) => {
+    const [isClient, setIsClient] = useState(false)
+ 
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+ 
     return (
+    <> {isClient ?         
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             {children}
-        </ThemeProvider>
-    );
+        </ThemeProvider> : 'Prerendered'}
+    </>)
 }
 
 export default Content;
@@ -15,7 +24,7 @@ export default Content;
 const theme = createTheme({
   palette: {
     background: {
-      default: '#f0f0f0',
+      default: '#ababa0',
       paper: '#fff',
     },
     text: {
