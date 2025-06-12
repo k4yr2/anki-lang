55688e26868a06cbf1c661a4bb0e5741426cd762
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 15:06:58 +0300

    feat: add HeaderMode component for theme switching in Header

--- next/src/component/Header.tsx
+++ next/src/component/Header.tsx
@@ -4,11 +4,14 @@ import { Settings as SettingsIcon } from "@deemlol/next-icons";
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button, Switch } from "@mui/joy";
+import { useColorScheme } from "@mui/joy/styles";
 
 const Header = () => {
     return (
         <header className={style.header}>
             <HeaderSettings />
+            <HeaderMode />
         </header>
     )
 };
@@ -26,3 +29,15 @@ const HeaderSettings = () => {
         </Link>
     )
 }
+
+const HeaderMode = () => {
+  const { mode, setMode } = useColorScheme();
+
+  return (
+    <Switch
+      variant="outlined"
+      onClick={() => setMode(mode === "light" ? "dark" : "light")}
+    >
+    </Switch>
+  );
+};
