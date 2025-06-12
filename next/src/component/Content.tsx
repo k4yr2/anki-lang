bad5cd81d38b8af32eccbe286197fac15cf8223d
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 15:06:21 +0300

    feat: refactor Content component to use CssVarsProvider and extendTheme for improved styling

--- next/src/component/Content.tsx
+++ next/src/component/Content.tsx
@@ -1,35 +1,47 @@
 "use client";
 
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
 import { useState, useEffect } from "react";
+import { extendTheme } from '@mui/joy/styles';
+
+const theme = extendTheme({
+    colorSchemes: {
+    light: {
+      palette: {
+        background: {
+          body: "#99A9A9",
+          surface: "#8787aa",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        background: {
+          body: "#000000",    // örnek koyu mod rengi
+          surface: "#121212", // örnek koyu mod rengi
+        },
+      },
+    },
+  }
+});
 
 const Content = ({ children } : { children : React.ReactNode}) => {
     const [isClient, setIsClient] = useState(false)
- 
+
     useEffect(() => {
         setIsClient(true)
     }, [])
  
+    if(!isClient) return null;
+
     return (
-    <> {isClient ?         
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider> : 'Prerendered'}
-    </>)
+      <>
+        <CssVarsProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </CssVarsProvider>
+      </>
+    );
 }
 
 export default Content;
-
-const theme = createTheme({
-  palette: {
-    background: {
-      default: '#ababa0',
-      paper: '#fff',
-    },
-    text: {
-      primary: '#111',
-      secondary: '#666',
-    },
-  },
-});
