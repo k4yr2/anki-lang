733aa5037f54b785e09dfcee2abf3bb76738f394
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 21:47:58 +0300

    feat: replace OpenAI Key input layout with SettingsPanel component in SettingsPage

--- next/app/settings/page.tsx
+++ next/app/settings/page.tsx
@@ -1,21 +1,11 @@
 "use client";
-import { FormControl, FormLabel, Grid, Input } from "@mui/joy";
-import { SxProps } from "@mui/joy/styles/types";
-import Link from "next/link";
+import SettingsPanel from "@/component/SettingsPanel";
 
 export default function SettingsPage() {
-  const labelSx : SxProps = { display: 'flex', justifyContent: { xs: 'flex-start', sm: 'flex-end' } };
 
   return (
     <div>
-        <Grid container rowSpacing={1} columnSpacing={2} alignItems="center">
-          <Grid xs={12} sm={2} sx={labelSx}>
-            <FormLabel htmlFor="openai-key">OpenAI Key</FormLabel>
-          </Grid>
-          <Grid xs={12} sm={10}>
-            <Input id="openai-key" placeholder="Enter your API Key" fullWidth />
-          </Grid>
-        </Grid>
+        <SettingsPanel />
     </div>
   );
 }
