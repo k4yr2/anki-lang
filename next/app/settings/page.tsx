bf218a5b3e4b60df4aee21189d08a4c7e7e1458e
k4yr2 <kyrzkaya@gmail.com>
2025-06-12 21:37:27 +0300

    feat: update layout for OpenAI Key input in SettingsPage for better responsiveness

--- next/app/settings/page.tsx
+++ next/app/settings/page.tsx
@@ -4,16 +4,16 @@ import { SxProps } from "@mui/joy/styles/types";
 import Link from "next/link";
 
 export default function SettingsPage() {
-  const labelSx : SxProps = { display: 'flex', justifyContent: 'right' };
+  const labelSx : SxProps = { display: 'flex', justifyContent: { xs: 'flex-start', sm: 'flex-end' } };
 
   return (
     <div>
         <Grid container rowSpacing={1} columnSpacing={2} alignItems="center">
-          <Grid xs={2} sx={labelSx}>
+          <Grid xs={12} sm={3} sx={labelSx}>
             <FormLabel>OpenAI Key</FormLabel>
           </Grid>
-          <Grid xs={10}>
-            <Input placeholder="Enter your API Key" />
+          <Grid xs={12} sm={9}>
+            <Input placeholder="Enter your API Key" fullWidth/>
           </Grid>
         </Grid>
     </div>
