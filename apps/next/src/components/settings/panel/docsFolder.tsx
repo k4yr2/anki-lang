1c9c22e0177af9e843483ebc5d0cce84da9c503a
k4yr2 <kyrzkaya@gmail.com>
2025-06-13 17:56:23 +0300

    feat: enhance StorageDir component with folder selection using IconButton

--- apps/next/src/components/settings/panel/docsFolder.tsx
+++ apps/next/src/components/settings/panel/docsFolder.tsx
@@ -1,8 +1,9 @@
-import { Button, FormControl, FormLabel, Grid, Input } from "@mui/joy";
+import { FormControl, FormLabel, Grid, IconButton, Input } from "@mui/joy";
 import { settingsPanel_labelSx as labelSx } from "./main";
 import { settingsPanel_labelSize as labelSize } from "./main";
 import { settingsPanel_inputSize as inputSize } from "./main";
 import { useState } from "react";
+import FolderIcon from '@mui/icons-material/Folder';
 
 export const StorageDir = () => {
         const [text, setText] = useState("");
@@ -13,8 +14,9 @@ export const StorageDir = () => {
 
         const handlePickFolder = async () => {
             const folder = await window.api.dialog.pickFolder();
+
             if (folder) {
-                console.log("Selected folder:", folder);
+                setText(folder);
             }
         };
     
@@ -24,20 +26,20 @@ export const StorageDir = () => {
                 <FormLabel htmlFor="docs-folder">Docs Folder</FormLabel>
             </Grid>
             <Grid xs={inputSize.xs} sm={inputSize.sm}>
-                <FormControl>
+                <FormControl orientation="horizontal" sx={{ display: 'flex', gap: 1 }}>
                     <Input
                         value={text}
                         onChange={handleTextChange}
-                        size="md" id="docs-folder" placeholder="Select a folder" fullWidth 
-                        endDecorator={
-                            <Button 
+                        size="md" id="docs-folder" placeholder="Select a folder" readOnly fullWidth
+                        startDecorator= {
+                            <IconButton 
                                 variant="soft" 
                                 color="neutral" 
-                                sx={{width: 80}}
                                 onClick={handlePickFolder}
+                                size="sm"
                             >
-                                Select
-                            </Button>
+                                <FolderIcon />
+                            </IconButton>
                         }
                     />
                 </FormControl>
